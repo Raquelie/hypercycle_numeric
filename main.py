c02701ec20928a6edc76be28edcc60c8bcd3385e
Raquelie <raqueliebbb@gmail.com>
2024-11-29 14:37:07 +0100

    add main file

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+
+def main():
+    """
+    Función principal del programa.
+    """
+    print("Bienvenido a mi programa.")
+    
+
+# Punto de entrada del programa
+if __name__ == "__main__":
+    main()
